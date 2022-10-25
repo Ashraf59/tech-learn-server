4463b1fd5ea6cc04529be7e0fa028b16bf13a2bf
Ashraf59 <ashrafams59b@gmail.com>
2022-10-25 17:25:45 +0600

    Json file image completed

--- index.js
+++ index.js
@@ -5,9 +5,41 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
+// console.log(categories)
+
 app.get('/', (req, res) => {
     res.send('Tech-Learn API running');
 });
+
+
+
+app.get('/course-categories', (req, res) => {
+    res.send(categories);
+})
+
+app.get('/category/:id', (req, res) =>{
+    const id = req.params.id;
+    
+    {
+        const category_course = courses.filter(n => n.category_id === id);
+    res.send(category_course);
+    console.log('from category', category_course);
+    }
+    
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) => {
+    console.log(req.params.id)
+    const id = req.params.id;
+    const selectedCourses = courses.find(n => n._id === id);
+    res.send(selectedCourses)
+})
 
 app.listen(port, () => {
     console.log('Tech-Learn website is running on Port', port)
